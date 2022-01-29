d58df899b4f85c9c909cddeb5ff3054fc47076a1
Bruno Barbosa <brb.dev@outlook.com>
2022-01-29 11:12:24 -0400

    encurtador

---
+++ src/controller/URLController.ts
@@ -0,0 +1,45 @@
+import { config } from '../config/Constants';
+import { Request, Response } from 'express';
+import shortId from 'shortId';
+import { URLModel } from '../database/model/URL';
+
+
+export class URLController {
+	public async shorten(req: Request, res: Response): Promise<void> {
+
+		// Verificar se a URL já existe
+		const { originURL } = req.body;
+		const url = await URLModel.findOne({ originURL });
+		if(url) {
+			res.json(url);
+			return
+		}
+
+		// Criar hash para a URL
+		const hash = shortId.generate();
+		const shortURL = `${config.API_URL}/${hash}`;
+
+		// Salvar a URL no banco
+		const newURL = await URLModel.create({ hash, shortURL, originURL });
+
+		// Retornar a URL encurtada
+		res.json({ newURL })
+	}
+
+	public async redirect(req: Request, res: Response): Promise<void> {
+		
+		// Pegar o hash gerado
+		const { hash } = req.params;
+
+		// Buscar a URL original pelo hash
+		const url = await URLModel.findOne({ hash });
+
+		// Redirecionar para URL original encontrada no BD
+		if(url) {
+			res.redirect(url.originURL);
+			return;
+		}
+
+		res.status(400).json({ error: "URL not found"});
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import { URLController } from './controller/URLController';
 import express from 'express';
+import { MongoConnection } from './database/MongoConnection';
 const port = 5000;
 
 const api = express();
-api.listen(port, () => console.log(`SERVIDOR EXPRESS EXECUTANDO NA PORTA ${port}...`))
+api.use(express.json());
+
+const database = new MongoConnection();
+database.connect();
+
+const urlController = new URLController();
+
+api.post('/shorten', urlController.shorten);
+api.get('/:hash', urlController.redirect);
+
+api.listen(port, () => console.log(`SERVIDOR EXPRESS EXECUTANDO NA PORTA ${port}...`));
 
